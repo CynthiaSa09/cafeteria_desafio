d879936942a4442772a36f36be723e4efa8ae5ff
Cynthia <cinthya3259@gmail.com>
2021-04-18 15:34:36 -0600

    commit final

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,5 @@
 class DashboardsController < ApplicationController
   def index
-    @coffees_in_months = Coffee.group_by_month(created_at).count
+    @coffees_in_months = Coffee.group_by_month(:created_at).count
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,11 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import Chartkick from 'chartkick'
+import Hightcharts from 'highcharts'
+window.Highcharts = Hightcharts
+
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
